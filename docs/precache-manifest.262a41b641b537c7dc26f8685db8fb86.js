9d09158983540dda581f0e674fbdd5dd292bb91d
anajd <ana.jaime.diaz@gmail.com>
2019-05-13 10:14:46 +0200

    docs

---
+++ docs/precache-manifest.262a41b641b537c7dc26f8685db8fb86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3efb14cbaaaebe66aeaad314850a15",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31818afdac95b46e8cf5",
+    "url": "./static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ddcd4b655d797d362890",
+    "url": "./static/js/2.b2bd8612.chunk.js"
+  },
+  {
+    "revision": "31818afdac95b46e8cf5",
+    "url": "./static/js/main.ee52b403.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
